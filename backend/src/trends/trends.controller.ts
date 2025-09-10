7fad05dfcbc473c32597b5bfc2f9c875ea20dcab
Jatin Harkhani <123296710+jatinharkhani@users.noreply.github.com>
2025-09-10 08:31:07 +0530

    Update trends.controller.ts

--- backend/src/trends/trends.controller.ts
+++ backend/src/trends/trends.controller.ts
@@ -1,1 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { TrendsService } from './trends.service';
+
+@Controller('trends')
+export class TrendsController {
+  constructor(private readonly trendsService: TrendsService) {}
+
+  @Get()
+  getTrends() {
+    return this.trendsService.fetchLatestTrends();
+  }
+}
 
